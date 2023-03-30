ca95a2e5b71cad2110231df30766f7b86b723a12
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-03-30 11:22:36 -0600

    Tarea 1 finalizada, Menu Simple

--- Homeworks_Gibran.cpp
+++ Homeworks_Gibran.cpp
@@ -1,107 +1,243 @@
 
-#include <Windows.h>
 #include <iostream>
 using namespace std;
-void Ejercicio1(); 
-int main()
+void Tarea1();
+void PoN();
+void Sum2Dig();
+void DigPar();
+void NumAlto();
+void ParMinMax();
+
+
+
+void PoN()
 {
-	int HWChoice = 0;
-    cout << "Bienvenido el conjunto de actividades realizadas por Gibran Garcia Coss y Leon" << endl;
-    cout << "Durante la clase de Programacion 2 en la carrera de Ingenieria de programacion de videojuegos"<<endl;
-    cout << "Elija la tarea que quiera correr: \n1.Guess My Number\n2.-------" << endl;
-	cin >> HWChoice;
-	switch (HWChoice)
-	{
-	case 1:
-		system("cls");
-		Ejercicio1 (); 
-		break;
-	default:
-		cout << "Opcion no valida"<<endl;
-		break;
-	}
-
-}
-
-void Ejercicio1() {
-
-    srand(time(NULL));//Se utiliza el tiempo de la maquina (reloj interno) para siempre generar un numero diferente 
-    int NumRan = rand() % 300;
-    int intentos = 0;
-    int Respuesta = 0;
-    int RangoMax = NumRan + 5;
-    int RangoMin = NumRan - 5;
-    cout << "Bienvenido al *Guess My Numer*\n";
-
+    int Num = 0;
+    bool RepPoN = true;
     do
     {
+        cout << "Introduzca un numero para determinar si es positivo o negativo" << endl;
+        cin >> Num;
 
-        cout << "Introduzca el numero que cree que la maquina haya elegido del 0 al 300\n";
-        cout << "Modo Test: El numero random es: " << NumRan << endl;
-        cin >> Respuesta;
-        if (Respuesta > NumRan)
+        if (Num < 0)
         {
-            intentos = intentos + 1;
-
-            cout << "Tu numero es mas alto que el de la maquina\n";
-            system("Color 09");
-            for (int i = RangoMax; i >= NumRan; i--)
-            {
-                if (Respuesta == i)
-                {
-                    cout << "Estas muy cerca, sigue intentandolo" << endl;
-                }
-            }
-
+            cout << "Su numero es Negativo" << endl;
         }
-        else if (Respuesta < NumRan)
+        else if (Num > 0)
         {
-            intentos = intentos + 1;
-            cout << "Tu numero es mas bajo que el de la maquina\n";
-            system("Color 0C");
-            for (int i = RangoMin; i <= NumRan; i++)
-            {
-                if (Respuesta == i)
-                {
-                    cout << "Estas muy cerca, sigue intentandolo" << endl;
-                }
-            }
-
-        }
-        else if (Respuesta == NumRan)
-        {
-
-            intentos = intentos + 1;
-            if (intentos == 1)
-            {
-                cout << "FELICIDADES, encontraste el numero en el primer intento!!!!!";
-                system("Color 01"); //Recordatorio de que en esta funcion el primer caracter (despues de color) es el background y el segundo es el color del texto
-                Sleep(250);
-                system("Color 04");
-                Sleep(250);
-                system("Color 02");
-                Sleep(250);
-                system("Color 01");
-                Sleep(250);
-                system("Color 04");
-                Sleep(250);
-                system("Color 02");
-                Sleep(250);
-
-
-            }
-            else
-            {
-                system("Color 02");
-                cout << "Correcto! Tu numero de intentos fue de " << intentos << " Intentos";
-            }
-
+            cout << "Su numero es Positivo" << endl;
         }
         else
         {
-            cout << "Opcion no valida";
+            cout << "Numero no valido, intente de nuevo" << endl;
         }
-    } while (Respuesta != NumRan);
+    } while (RepPoN == true);
+
+}
+
+void Sum2Dig()
+{
+    int NumEnt = 0;
+    int NumEx = 0;
+    int SumNum = 0;
+    bool RepSum2Dig = true;
+    do
+    {
+        cout << "Introduzca un numero de dos digitos para sumar estos" << endl;
+        cin >> NumEnt; //Usemos como ejemplo el numero 25 
+
+        if (NumEnt > 99 || NumEnt < 10) //25 no aplica aqui asi que pasa a la siguiente linea
+        {
+            cout << "Numero no valido, tiene que ser de dos digitos" << endl;
+        }
+        else
+        {
+            while (NumEnt != 0) {
+                NumEx = NumEnt % 10;    //El modulo de 25/10 es 5, esto se guarda en NumEx
+                NumEnt = NumEnt / 10;     //El numero ingresado se iguala a su divsion entre 10 en entero en este caso es 2
+                SumNum = SumNum + NumEx;    //La variable SumNum se iguala a esta misma mas el 5 de NumEx
+            }                               //Se repite esto hasta que el numero ingresado sean puros decimales al dividirse por 10
+                                            //Lo cual como es un *int* va a quedar en 0 cuando no queden digitos
+            cout << "La suma de los 2 digitos es de: " << SumNum << endl;//La respuesta seria 7
+        }
+
+    } while (RepSum2Dig == true);//Se sigue repitiendo hasta que le meta una opcion para continuar 
+
+
+}
+
+void DigPar()
+{
+    int NumEnt = 0;
+    int NumDerecha = 0;
+    int NumIzquierda = 0;
+
+    bool RepDigPar = true;
+    do
+    {
+        cout << "Introduzca un numero de dos digitos para determinar cual de los 2 es par" << endl;
+        cin >> NumEnt;
+
+        if (NumEnt > 99 || NumEnt < 10)
+        {
+            cout << "Numero no valido, tiene que ser de dos digitos" << endl;
+        }
+        else
+        {
+
+            NumDerecha = NumEnt % 10;   ///Numero de la derecha se guarda aqui 
+            NumIzquierda = NumEnt / 10;   //El de la izquierda se guarda aqui usando la misma logica que del ejercicio anterior  
+
+            if (NumDerecha % 2 == 0 && NumIzquierda % 2 == 0)
+            {
+                cout << " !!!!!Ambos digitos son par!!!! " << endl;
+            }
+            else if (NumDerecha % 2 == 0)
+            {
+                cout << "El numero " << NumDerecha << " es par" << endl;
+            }
+            else if (NumIzquierda % 2 == 0)
+            {
+                cout << "El numero " << NumIzquierda << " es par" << endl;
+            }
+            else
+            {
+                cout << "Ninguno es par" << endl;
+            }
+
+
+        }
+
+    } while (RepDigPar == true);
+}
+
+void NumAlto()
+{
+    int Num1 = 0;
+    int Num2 = 0;
+    int Num3 = 0;
+    bool RepNumAlto = true;
+    do
+    {
+        cout << "Introduzca tres numeros para determinar cual es el mayor" << endl;
+        cout << "Introduzca el 1er numero" << endl;
+        cin >> Num1;
+        cout << "Introduzca el 2do numero" << endl;
+        cin >> Num2;
+        cout << "Introduzca el 3er numero" << endl;
+        cin >> Num3;
+
+        if (Num1 > Num2 && Num1 > Num3)
+        {
+            cout << "El numero mas grande es: " << Num1 << endl;
+        }
+        else if (Num2 > Num1 && Num2 > Num3)
+        {
+            cout << "El numero mas grande es: " << Num2 << endl;
+        }
+        else if (Num3 > Num1 && Num3 > Num2)
+        {
+            cout << "El numero mas grande es: " << Num3 << endl;
+        }
+
+
+    } while (RepNumAlto == true);
+}
+
+void ParMinMax()
+{
+    int Min = 0;
+    int Max = 0;
+    bool RepParMinMax = true;
+
+    do
+    {
+        cout << "Introduzca el rango Maximo" << endl;
+        cin >> Max;
+        cout << "Introduzca el rango Minimo" << endl;
+        cin >> Min;
+        cout << "Los numero pares del " << Min << " al " << Max << " son:" << endl;
+        for (int i = Min; i <= Max; i++)
+        {
+
+            if (i % 2 == 0)
+            {
+
+                cout << i << ", ";
+            }
+        }
+        cout << endl;
+    } while (RepParMinMax == true);
+
+}
+
+
+void Tarea1()
+{
+    int MenuTarea1 = 0;
+    cout << "-----------Elija que ejercicio desea ver-----------" << endl;
+    cout << "1. Determinar si un numero es positivo o negativo" << endl;
+    cout << "2. Suma de los 2 digitos de un numero dado" << endl;
+    cout << "3. Determinar si los digitos de un numero son par o impar" << endl;
+    cout << "4. Determinar que numero es el mas alto de 3 numeros" << endl;
+    cout << "5. Todos los numeros pares dentro de un rango dado" << endl;
+    cin >> MenuTarea1;
+    switch (MenuTarea1)
+    {
+    case 1:
+        PoN();
+        break;
+
+    case 2:
+        Sum2Dig();
+        break;
+
+    case 3:
+        DigPar();
+        break;
+
+    case 4:
+        NumAlto();
+        break;
+    case 5:
+        ParMinMax();
+        break;
+    default:
+        cout << "Opcion no valida";
+        break;
+
+    }
+
+}
+
+int main()
+{
+    int HWChoice = 0;
+    bool ExitMain = false;
+
+    do
+    {
+        cout << "Bienvenido el conjunto de tareas realizadas por Gibran Garcia Coss y Leon" << endl;
+        cout << "Durante la clase de Programacion 2 en la carrera de Ingenieria de programacion de videojuegos" << endl;
+        cout << "Elija la tarea que quiera correr: \n1.Tarea 1\n2.Salir" << endl;
+        cin >> HWChoice;
+
+        switch (HWChoice)
+        {
+        case 1:
+            system("cls");
+            Tarea1();
+            break;
+
+        case 2:
+            ExitMain = true;
+            break;
+
+        default:
+            cout << "Opcion no valida" << endl;
+        }
+
+    } while (ExitMain == false);
 
 }
 
